cdf532640f9c8db8bd8efe497d269a39cb059e3a
Administrator <1095978434@qq.com>
2019-09-20 21:14:09 +0800

    先把项目都在本地提交上好

---
+++ src/main/java/com/example/asddd/AsdddApplication.java
@@ -0,0 +1,13 @@
+package com.example.asddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsdddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsdddApplication.class, args);
+    }
+
+}
